80507a5a93050fa49897d3f2b32723a904d757ae
Pratik Bhattacharjee <pratikbhattacharjee09@gmail.com>
2024-02-29 11:37:11 +0800

    Project initialised

---
+++ src/main/java/com/pratikbhattacharjee/SsecurityMULTIPLEAuth/SsecurityMultipleAuthApplication.java
@@ -0,0 +1,13 @@
+package com.pratikbhattacharjee.SsecurityMULTIPLEAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsecurityMultipleAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsecurityMultipleAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratikbhattacharjee/SsecurityMULTIPLEAuth/SsecurityMultipleAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratikbhattacharjee.SsecurityMULTIPLEAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsecurityMultipleAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
